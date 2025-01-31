2c1445ab81ce526bd5d59e96b09364792eef574d
serg-n3 <ktg80@yandex.ru>
2025-01-31 16:09:47 +0300

    4.1. Разбор первой программы. [#159033 #337147]

---
+++ src/main/java/ru/job4j/Multiple.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class Multiple {
+    public static void main(String[] args) {
+        System.out.println("1 + 1 = 2");
+        System.out.println("2 + 1 = 3");
+        System.out.println("3 + 1 = 4");
+    }
+}
